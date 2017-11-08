7bb353bb024a8440dca39764aeb7742f975f0e44
tsunchen <tsunchen@users.noreply.github.com>
2017-11-08 21:24:29 +0800

    Update class_snmppyget.py

--- class_snmppyget.py
+++ class_snmppyget.py
@@ -23,7 +23,7 @@ class Snmp(object):
 '''
 
 
-# reference python for linux and unix administration 
+# reference python for linux and unix administration
 # pysnmp classification
 class Snmppyget(object):
     """A basic SNMP session"""
@@ -38,12 +38,12 @@ class Snmppyget(object):
         """Creates SNMP query session"""
         try:
             #cg = cmdgen.CommandGenerator()
-            errorIndication, errorStatus, errorIndex, varBinds = self.cg.getCmd( 
+            errorIndication, errorStatus, errorIndex, varBinds = self.cg.getCmd(
                 cmdgen.CommunityData('Agent', self.community, self.version),
                 cmdgen.UdpTransportTarget((self.destHost, 161)),
                 self.mid
-            ) 
-            result = varBinds[0]
+            )
+            result = varBinds[0][1]
         except Exception, err:
             print err
             result = None
@@ -51,9 +51,21 @@ class Snmppyget(object):
 
 def runit(loop=1):
     for i in range(loop):
-        s = Snmppyget('pub', '211.152', '1.3.6.1.2.1.1.5.0')
-        port = s.query()
-        print port
+        s = Snmppyget('publ1c', '211.152.50.254', '1.3.6.1.2.1.1.5.0')
+        hostname = s.query()
+        print hostname
+
+        s = Snmppyget('publ1c', '211.152.50.254', '1.3.6.1.2.1.2.2.1.2.15')
+        ifDesc = s.query()
+        print ifDesc
+
+        s = Snmppyget('publ1c', '211.152.50.254', '1.3.6.1.2.1.2.2.1.10.15')
+        ifIn = s.query()
+        print ifIn
+
+        s = Snmppyget('publ1c', '211.152.50.254', '1.3.6.1.2.1.2.2.1.16.15')
+        ifOut = s.query()
+        print ifOut
         #print i
 
 if __name__ == "__main__":
